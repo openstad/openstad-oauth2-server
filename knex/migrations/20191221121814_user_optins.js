0353e5cc31204920549b2d3f157dddc0153ba725
stijnvdvegt <stijnvandervegt@gmail.com>
2019-12-21 12:22:44 +0100

    add user_optins migration

---
+++ knex/migrations/20191221121814_user_optins.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('user_optins', function(table) {
+    table.increments();
+    table.integer('userId').unsigned().notNullable().references('id').inTable('users');
+    table.string('optin').index();
+    table.timestamp('createdAt').defaultTo(knex.fn.now());
+    table.timestamp('updatedAt').defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex, Promise) {
+  knex.schema.dropTable('user_optins');
+};
